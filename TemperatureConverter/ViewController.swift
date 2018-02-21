868479729eaf3210e8610d978408ac0a69c2431d
Jeannette <jeannette@server.strandacq.ca>
2018-02-20 21:31:48 -0800

    closure implemented

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -13,26 +13,40 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var valField: UITextField!
     @IBOutlet weak var valResult: UITextField!
+   
     @IBOutlet weak var nameLabel: UILabel!
-    var conversion : Conversion
+    @IBOutlet weak var buttonA: UIButton!
+    @IBOutlet weak var buttonB: UIButton!
+    var item: Conversion?
+    
     
     // Mark: Methods
     // Button A invokes the closureA to convert units
     @IBAction func buttonA(_ sender: UIButton) {
         let value = Double(self.valField.text!)
-        self.valResult.text = ((value) != nil) ? String(conversion.closureA(value!)) : "N/A"
+        self.valResult.text = ((value) != nil) ? String(item!.closureA(value!)) : "N/A"
     }
     // Button B invokes the closureB to convert units
     @IBAction func buttonB(_ sender: UIButton) {
         let value = Double(self.valField.text!)
-        self.valResult.text = ((value) != nil) ? String(conversion.closureB(value!)) : "N/A"
+        self.valResult.text = ((value) != nil) ? String(item!.closureB(value!)) : "N/A"
     }
     
     // MARK: Delegate Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.valField.delegate = self
+        
+        if let item = item {
+            self.valField.delegate = self
+            nameLabel.text = item.nameLabel
+            buttonA.setTitle(item.buttonLabelA, for: UIControlState.normal)
+            buttonB.setTitle(item.buttonLabelB, for: UIControlState.normal)
+        } else {
+            nameLabel.text = "Error"
+            buttonA.setTitle("N/A", for: UIControlState.normal)
+            buttonB.setTitle("N/A", for: UIControlState.normal)
+        }
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
